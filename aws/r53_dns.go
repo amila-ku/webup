aac9812fb3f397079fbe49612edfad13df02a766
amila-ku <amila.kumaranayaka@gmail.com>
2021-10-13 00:07:11 +0200

    adding route creation

--- aws/r53_dns.go
+++ aws/r53_dns.go
@@ -1,11 +1,14 @@
 package aws
 
 import (
-	"fmt"
 	"context"
 	"log"
-	r53 "github.com/aws/aws-sdk-go-v2/service/route53"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	r53 "github.com/aws/aws-sdk-go-v2/service/route53"
+	r53types "github.com/aws/aws-sdk-go-v2/service/route53/types"
+	"github.com/aws/aws-sdk-go/service/route53"
 )
 
 // CreateHostedZoneAPI defines the interface for the CreateHostedZone function.
@@ -14,6 +17,14 @@ type R53CreateHostedZoneAPI interface {
 	CreateHostedZone(ctx context.Context,
 		params *r53.CreateHostedZoneInput,
 		optFns ...func(*r53.Options)) (*r53.CreateHostedZoneOutput, error)
+}
+
+// ChangeResourceRecordSetsAPI defines the interface for the CreateHostedZone function.
+// We use this interface to test the function using a mocked service.
+type R53ChangeResourceRecordSetsAPI interface {
+	ChangeResourceRecordSets(ctx context.Context,
+		params *r53.ChangeResourceRecordSetsInput,
+		optFns ...func(*r53.Options)) (*r53.ChangeResourceRecordSetsOutput, error)
 }
 
 func NewR53Client() (*r53.Client, error) {
@@ -31,8 +42,29 @@ func NewR53Client() (*r53.Client, error) {
 
 func MakeRoutes() error {
 	
+	input := &r53.ChangeResourceRecordSetsInput{
+		ChangeBatch: &r53types.ChangeBatch{
+			Changes: []*r53types.Change{
+				{
+					Action: aws.String("CREATE"),
+					ResourceRecordSet: &route53.ResourceRecordSet{
+						Name: aws.String("example.com"),
+						ResourceRecords: []*route53.ResourceRecord{
+							{
+								Value: aws.String("192.0.2.44"),
+							},
+						},
+						TTL:  aws.Int64(60),
+						Type: aws.String("A"),
+					},
+				},
+			},
+			Comment: aws.String("Web server for example.com"),
+		},
+		HostedZoneId: aws.String("Z3M3LMPEXAMPLE"),
+	}
 }
 
-func createRoute(c context.Context, api R53CreateHostedZoneAPI, input *r53.CreateHostedZoneInput) (*r53.CreateHostedZoneOutput, error) {
-	return api.CreateHostedZone(c, input )
+func createRoute(c context.Context, api R53ChangeResourceRecordSetsAPI, input *r53.ChangeResourceRecordSetsInput, hostedzone string) (*r53.ChangeResourceRecordSetsOutput, error) {
+	return api.ChangeResourceRecordSets(c, input )
 }
