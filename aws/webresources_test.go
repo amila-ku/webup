50c543640a472721ea69d1a078f62831bc325942
amila-ku <amila.kumaranayaka@gmail.com>
2021-11-30 18:35:32 +0100

    add tests covering resource creation

---
+++ aws/webresources_test.go
@@ -0,0 +1,30 @@
+package aws
+
+import (
+	"context"
+	"testing"
+
+	// r53 "github.com/aws/aws-sdk-go-v2/service/route53"
+	// r53types "github.com/aws/aws-sdk-go-v2/service/route53/types"
+	// "github.com/aws/smithy-go/middleware"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMakeWebResources(t *testing.T) {
+
+	// creates a s3 client
+	s3client := &S3BucketImpl{}
+
+	// creates s3 bucket and setup bucket for webhosting
+	bucket, err := MakeBucket(context.TODO(), s3client, "abc.com")
+	assert.Nil(t, err)
+	assert.Equal(t, "abc.com", bucket )
+
+	// creates a r53 client
+	r53client := &R53APIImpl{}
+
+	// create route53 rules
+	dns, err := MakeRoutes(context.TODO(), r53client, "s3-website-eu-west-1.amazonaws.com", "abc-test.com", "Z1TI4H711TUAOG")
+	assert.Nil(t, err)
+	assert.Equal(t, "abc-test.com", dns)
+}
